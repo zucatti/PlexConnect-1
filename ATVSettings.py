b72bacd69fd13373c9e86cf857cf3f32fa6923d9
CyberGhost84 <yvesflammang@hotmail.com>
2014-08-21 18:02:28 +0200

    update

--- ATVSettings.py
+++ ATVSettings.py
@@ -21,6 +21,7 @@ options = { \
     'plex_movieview'                :('Flow', 'Paged Grid', 'Grid', 'List', 'Detailed List'), \
     'plex_homevideoview'            :('Flow', 'Paged Grid', 'Grid', 'List', 'Detailed List'), \
     'plex_showview'                 :('Flow', 'Paged Grid', 'Grid', 'List', 'Bookcase'), \
+    'plex_episodeview'              :('List', 'Paged Grid'), \
     'plex_channelview'              :('Grid', 'List', 'Bookcase'), \
     'plex_sectionicons'              :('Fanart', 'Apple', 'Plex', 'Custom'), \
     'plex_sectionicons_shared'       :('Fanart', 'Apple', 'Plex'), \
